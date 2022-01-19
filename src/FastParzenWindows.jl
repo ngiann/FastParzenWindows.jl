fdcb69fd9c6f69f9f77b60068b263ba5b0307260
ngiann <ngiann@gmail.com>
2022-01-19 21:31:38 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.26

---
+++ src/FastParzenWindows.jl
@@ -0,0 +1,5 @@
+module FastParzenWindows
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using FastParzenWindows
+using Test
+
+@testset "FastParzenWindows.jl" begin
+    # Write your tests here.
+end
