f8531ff136f42864f646d8d91a99aa97516b30b8
ngiann <ngiann@gmail.com>
2022-01-20 09:00:25 +0100

    minor

--- src/fastparzenwindows.jl
+++ src/fastparzenwindows.jl
@@ -17,10 +17,6 @@ fpw(X, r) = fastparzenwindows(X, r)
 
 function fastparzenwindows(X, r)
 
-  N = size(X, 1)
-
-  D = size(X, 2)
-
   centres_ind = partition(X, r)
 
   Q, mu, C = softparzen(X, centres_ind, r)
