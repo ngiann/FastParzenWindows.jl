34b5911d0276eae2ed93bc18629735e6036c59da
ngiann <ngiann@gmail.com>
2023-01-19 15:10:28 +0100

    modify docstrings

--- src/cvfpw.jl
+++ src/cvfpw.jl
@@ -1,17 +1,30 @@
 """
-    cvresults = cv_fpw(X, r_range; numfolds = 10, seed = 1, gamma = 1e-6, randrepeats = 7)
+  cvresults = cvfpw(X, r_range; numfolds = 10, seed = 1, gamma = 1e-6, randrepeats = 7)
 
-Performs cross validation for the radius parameter.
+Performs cross validation for the radius parameter for partitioning the data in hyperdiscs.
 Candidate values for the radius parameter are specified in `r_range`.
-Seed controls the generation of the folds.
-Returns the a matrix of dimensions (number of radii candidates)×(number of folds) of log-likelihoods evaluated on left out folds.
+Seed controls the generation of the folds and is also the random seed of `fpw`.
+Returns a matrix of dimensions (number of radii candidates)×(number of folds) of log-likelihoods evaluated on left out folds.
+
+# Parameters
+
+* `X` is a N×D data matrix, i.e. there are N data items of dimension D.
+* `r_range` is an array or range of scalars specifying the candidate radii.
+* `seed` controls the generation of the folds and is also the random seed of`fpw`
+* `gamma` is a scalar that specified a multiple of the identity matrix, i.e. γI, added to the covariance matrices of the local Gaussian for numerical stability.
+* `randrepeats` specifies how many times to repeat running the `fpw` algorithm in order to take into account the random initialisation of `fpw` each time it is run.
+
+# Returns
+
+* `cvresults` is a matrix of dimensions (number of radii candidates)×(number of folds) of log-likelihoods evaluated on left out folds.
+
 
 # Example
 ```julia-repl
 julia> using Statistics, PyPlot
 julia> X = spiraldata(300)
 julia> r_range = LinRange(0.01, 2.0, 100)
-julia> cvresults =  cv_fpw(X, r_range)
+julia> cvresults =  cvfpw(X, r_range)
 julia> r_perf = mean(cvresults, dims=2)
 julia> best_index = argmax(r_perf)
 julia> r_best = r_range[best_index]
@@ -22,7 +35,7 @@ julia> plot(x[:,1], x[:,2], ".r", label="generated", alpha=0.7)
 julia> legend()
 ```
 """
-function cv_fpw(X, r_range; numfolds = 10, seed = 1, gamma = 1e-6, randrepeats = 7)
+function cvfpw(X, r_range; numfolds = 10, seed = 1, gamma = 1e-6, randrepeats = 7)
 
 
   Random.seed!(seed)
--- src/fpw.jl
+++ src/fpw.jl
@@ -1,7 +1,25 @@
 """
-    fpw(X, r; gamma = 1e-6, seed = 1)
 
-ntrols the random number generator used for initialising the starting point of the optimisation.
+  p = fpw(X, r; gamma = 1e-6, seed = 1)
+
+Estimate density through the fast parzen windows density algorithm. 
+The algorithm partitions the data space in hyperdiscs of radius `r`.
+Data items in matrix `X` are then 'softly' assigned to the partitions.
+The local density in each partition is modelled by a Gaussian distribution.
+The global density estimate is returned as a Gaussian mixture model of type `Distributions.MixtureModel`.
+
+The implementation is based on **X. Wang, P. Tino, M. A. Fardal, S. Raychaudhury and A. Babul, "Fast parzen window density estimator," 2009 International Joint Conference on Neural Networks, 2009, pp. 3267-3274.**
+
+# Parameters
+
+* `X` is a N×D data matrix, i.e. there are N data items of dimension D.
+* `r` is a scalar specifying the common radius of the hyperdiscs
+* `seed` controls the random number generator that randomly picks data items as hyperdisc centres.
+* `gamma` is a scalar that specified a multiple of the identity matrix, i.e. γI, added to the covariance matrices of the local Gaussian for numerical stability.
+
+# Returns
+
+* `p` a Gaussian mixture model as type `Distributions.MixtureModel`
 
 # Example
 ```julia-repl
